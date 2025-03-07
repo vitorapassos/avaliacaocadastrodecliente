de9d6a57f4a93018efff3f471897bdf5746a9f32
VITOR DE ASSIS PASSOS <vitor.apassos@senacsp.edu.br>
2025-03-07 20:05:39 -0300

    Primeira parte cadastro de cliente

--- main.js
+++ main.js
@@ -7,31 +7,55 @@ const { pipeline } = require("responselike")
 
 console.log("Executando processo principal")
 
-const { app, BrowserWindow, nativeTheme, Menu, Shell} = require('electron/main')
+const { app, BrowserWindow, nativeTheme, Menu, shell } = require('electron/main')
 
 let win
 
 const createWindow = () => {
-    nativeTheme.themeSource = 'light'
+    nativeTheme.themeSource = 'dark'
     win = new BrowserWindow({
         width: 1010,
         height: 720
     })
 
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+
     win.loadFile('./src/views/index.html')
 }
 
+function aboutWindow(){
+    nativeTheme.themeSource = 'light'
+
+    const mainWindow = BrowserWindow.getFocusedWindow()
+
+
+    if(mainWindow){
+        about = new BrowserWindow({
+            width: 320,
+            height: 300,
+            autoHideMenuBar: true,
+            resizable: false,
+            minimizable: false,
+            parent: mainWindow,
+            modal: true
+        })
+    }
+
+    about.loadFile('./src/views/sobre.html')
+}
+
+// Inicialização
 app.whenReady().then(() => {
     createWindow()
 
     app.on('activate', () => {
-        if(BrowserWindow.getAllWindows().length === 0){
+        if (BrowserWindow.getAllWindows().length === 0) {
             createWindow()
         }
     })
 })
 
-app.on('window-all-closed', () =>{
+app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit
     }
@@ -45,7 +69,58 @@ const template = [
         label: 'Cadastro',
         submenu: [
             {
-                label: Sair
+                label: 'Cadastrar'
+            },
+            {
+                label: 'Sair',
+                accelerator: 'Alt+F4',
+                click: () => app.quit()
+            }
+        ]
+    },
+    {
+        label: 'Relatório',
+        submenu: [
+            {
+                label: 'Clientes'
+            }
+
+        ]
+    },
+    {
+        label: 'Ferramentas',
+        submenu: [
+            {
+                label: 'Aplicar Zoom',
+                role: 'ZoomIn'
+            },
+            {
+                label: 'Reduzir Zoom',
+                role: 'ZoomOut'
+            },
+            {
+                label: 'Restaurar Zoom',
+                role: 'resetZoom'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'DevTools',
+                role: 'ToggleDevTools'
+            }
+        ]
+    },
+    {
+        label: 'Ajuda',
+        submenu: [
+            {
+                label: 'Repositório',
+                click: () => shell.openExternal('https://github.com/vitorapassos/avaliacaocadastrodecliente.git')
+            },
+            {
+                label: 'Sobre',
+                click: () => aboutWindow()
             }
         ]
     }
